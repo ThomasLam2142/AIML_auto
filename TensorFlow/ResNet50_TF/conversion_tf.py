d36d9fd09cc3ece5c06ee080fc9bfdc7f76adef2
bbtruong <85854256+bbtruong@users.noreply.github.com>
2024-03-27 15:35:44 -0400

    Add files via upload

--- TensorFlow/ResNet50_TF/conversion_tf.py
+++ TensorFlow/ResNet50_TF/conversion_tf.py
@@ -1,14 +1,15 @@
 import tensorflow as tf
+import tf2onnx
 import onnx
-import onnx_tf
 
-# Load the TensorFlow ResNet model
+# Load the pre-trained ResNet50 model from TensorFlow
 model = tf.keras.applications.ResNet50(weights='imagenet')
 
-# Convert TensorFlow model to ONNX
-onnx_model = onnx_tf.convert.from_keras(model)
+# Convert the TensorFlow model to ONNX
+onnx_model, _ = tf2onnx.convert.from_keras(model)
 
-# Save the ONNX model to a file
-onnx.save(onnx_model, 'resnet50.onnx')
+# Save the ONNX model
+onnx_file_path = 'resnet50_model.onnx'
+onnx.save_model(onnx_model, onnx_file_path)
 
-print("TensorFlow ResNet50 model converted to ONNX successfully.")
+print("ResNet50 model successfully converted to ONNX format and saved as:", onnx_file_path)
---
+++ TensorFlow/ResNet50_TF/inference_onnx_tf.py
@@ -0,0 +1,45 @@
+import numpy as np
+import onnxruntime
+import time
+from PIL import Image
+import tensorflow as tf
+from tensorflow.keras.applications.resnet50 import preprocess_input
+
+# Read the categories
+with open("imagenet_classes.txt", "r") as f:
+    categories = [s.strip() for s in f.readlines()]
+
+# Pre-process the image for ResNet-50 inference
+filename = 'cat.jpg'  # change to your filename
+input_image = Image.open(filename)
+input_image = input_image.resize((224, 224))  # Resize to 224x224
+input_array = np.asarray(input_image)
+input_array = preprocess_input(input_array)  # Preprocess the input image
+
+# Convert the input to a tensor
+input_tensor = tf.convert_to_tensor(input_array, dtype=tf.float32)
+input_tensor = tf.expand_dims(input_tensor, 0)  # Add batch dimension
+
+# Set Execution Provider for ONNX Runtime
+session_fp32 = onnxruntime.InferenceSession("resnet50_model.onnx")
+
+# warm up run to load model and data
+_ = session_fp32.run(None, {'input_1': input_tensor.numpy()})
+
+# Run inference with ONNX Runtime
+latency = []
+start = time.time()
+ort_outputs = session_fp32.run(None, {'input_1': input_tensor.numpy()})[0]
+end = time.time()
+latency.append(end - start)
+
+# Process ONNX Runtime outputs
+output = ort_outputs.flatten()
+output = np.exp(output - np.max(output)) / np.sum(np.exp(output - np.max(output)))  # softmax
+top5_catid = np.argsort(-output)[:5]
+
+# Show top categories per image
+for i in range(len(top5_catid)):
+    print(categories[top5_catid[i]], output[top5_catid[i]])
+
+print("ONNX Inference Time = {} ms\n".format(format(sum(latency) * 1000 / len(latency), '.2f')))
