c3da72a6d9d8885fc618e1d9ea5428608793b848
bbtruong <85854256+bbtruong@users.noreply.github.com>
2024-05-14 10:48:30 -0400

    Defined test_data

--- ResNet50/ResNet50_Benchmark/conversion_onnx_mixed.py
+++ ResNet50/ResNet50_Benchmark/conversion_onnx_mixed.py
@@ -1,11 +1,17 @@
-#pip install onnxconverter-common before using script
-
-from onnxconverter_common import auto_mixed_precision
 import onnx
+import torch
+import numpy as np
+from onnxconverter_common import auto_mixed_precision
+
+# Define the shape of the input tensor for ResNet50
+input_shape = (1, 3, 224, 244)
+
+# Create a sample input tensor in FP16
+test_input = np.random.rand(*input_shape).astype(np.float16)
+
+# Convert the test input into a PyTorch tensor
+test_data = torch.tensor(test_input)
 
 model = onnx.load("resnet50_model.onnx")
-model_fp16 = auto_convert_mixed_precision(model, test_data, rtol=0.01, atol=0.001, keep_io_types=True)
+model_fp16 = auto_mixed_precision.auto_convert_mixed_precision(model, test_data, rtol=0.01, atol=0.001, keep_io_types=True)
 onnx.save(model_fp16, "resnet50_model_mixed.onnx")
-
-# if converted model does not work or has poor accuracy, set these arguments in line 5:
-# auto_convert_mixed_precision(model, feed_dict, validate_fn=None, rtol=None, atol=None, keep_io_types=False)
