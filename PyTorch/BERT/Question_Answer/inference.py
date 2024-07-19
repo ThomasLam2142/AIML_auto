06fe6114b6aa21e72d0f64dbbd8387b72621df56
bbtruong <85854256+bbtruong@users.noreply.github.com>
2024-07-19 09:43:14 -0400

    Delete PyTorch directory
    Restructuring repo

--- PyTorch/BERT/Question_Answer/inference.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-question = "Where do I live?"
-context = "My name is Mern, and I live in Barcelona"
-
-question_answerer = pipeline("question-answering", model="distilbert_qa_model/checkpoint-500")
-output = question_answerer(question = question, context=context)
-print(output)
