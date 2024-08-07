180e3f56e79c49e2707b7d7dbc553e33f218ff7e
bbtruong <85854256+bbtruong@users.noreply.github.com>
2024-08-07 11:57:53 -0400

    Update inference_onnx.py
    -GPU entablement

--- pytorch/bert/question_answer/inference_onnx.py
+++ pytorch/bert/question_answer/inference_onnx.py
@@ -6,14 +6,14 @@ question = "What is my name?"
 context = "My name is Mern and I live in Barcelona"
 
 # Load the tokenizer
-tokenizer = AutoTokenizer.from_pretrained("distilbert_qa_model/checkpoint-750")
+tokenizer = AutoTokenizer.from_pretrained("distilbert_qa_model/checkpoint-500")
 
 # Tokenize the input text and return tensors
 inputs = tokenizer(question, context, return_tensors="pt")
 
 # Load the ONNX model
 onnx_path = "distilbert_qa_model.onnx"
-ort_session = ort.InferenceSession(onnx_path)
+ort_session = ort.InferenceSession(onnx_path, providers=["ROCMExecutionProvider"])
 
 # Run inference
 ort_inputs = {
