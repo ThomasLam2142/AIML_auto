9321f88eec5eae3704a2370f693f67fd6507ff05
bbtruong <85854256+bbtruong@users.noreply.github.com>
2024-11-08 11:13:31 -0500

    Update trainer.py for ORT functionality

--- pytorch/resnet50/resnet50_mgpu/utils/trainer.py
+++ pytorch/resnet50/resnet50_mgpu/utils/trainer.py
@@ -1,14 +1,16 @@
 import numpy as np
 import torch
 from utils.utils import AverageMeter, accuracy
+from onnxruntime.training import ORTModule
 
 class Trainer:
-    def __init__(self, model, criterion, optimizer, scheduler, scaler):
-        self.model = model
+    def __init__(self, model, criterion, optimizer, scheduler, scaler, use_ort):
+        self.model = ORTModule(model) if use_ort else model
         self.criterion = criterion
         self.optimizer = optimizer
         self.scheduler = scheduler
         self.scaler = scaler
+        self.use_ort = use_ort
 
     def train(self, data_loader, epoch, amp, log_interval, result_dict):
         total_loss = 0
@@ -22,6 +24,7 @@ class Trainer:
         for batch_idx, (inputs, labels) in enumerate(data_loader):
             inputs, labels = inputs.cuda(), labels.cuda()
 
+            # Forward pass
             if amp == 'Yes':
                 with torch.autocast("cuda"):
                     outputs = self.model(inputs)
@@ -39,6 +42,7 @@ class Trainer:
 
             self.optimizer.zero_grad()
 
+            # Backward pass
             if amp == 'Yes':
                 self.scaler.scale(loss).backward()
                 self.scaler.step(self.optimizer)
@@ -49,6 +53,7 @@ class Trainer:
             total_loss += loss.tolist()
             count += labels.size(0)
 
+            # Logging the result at set interval
             if batch_idx % log_interval == 0:
                 _s = str(len(str(len(data_loader.sampler))))
                 ret = [
