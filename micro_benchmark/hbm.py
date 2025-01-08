930c7a56e67aaf288b17561a765a53c94ce9eb62
taccuser <billytruong2000@gmail.com>
2025-01-08 16:14:10 -0500

    update comment

--- micro_benchmark/hbm.py
+++ micro_benchmark/hbm.py
@@ -4,7 +4,7 @@ import torch
 import triton
 
 # Tensor size (1GB)
-tensor_size = 2 * 1024**3
+tensor_size = 1 * 1024**3
 dtype = torch.float32
 num_elements = tensor_size
 
@@ -24,3 +24,5 @@ bandwidth_gbps = (tensor_size * 2) / (time_ms * 1e-3) / 1e9 #multiply by 2 for r
 
 # Print results
 print(f"Copy Bandwidth: {bandwidth_gbps:.2f} GB/s (time: {time_ms:.2f} ms)")
+
+# Need to have a closer look on the function of tensor copy, if its being copied within the memory or across CPU-PCIE-GPU
