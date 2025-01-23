0c6ff51c4aae7b020b9e2947a7f3212057693d6a
taccuser <billytruong2000@gmail.com>
2025-01-23 12:02:13 -0500

    fix quantize script

--- pytorch/bert/question_answer/quantize.py
+++ pytorch/bert/question_answer/quantize.py
@@ -1,8 +1,8 @@
 from onnxruntime.quantization import quantize_dynamic, QuantType
 
 # Path to the original ONNX model and the quantized output path
-onnx_model_path = "bert_tc_model.onnx"
-quantized_model_path = "bert_tc_model_int8.onnx"
+onnx_model_path = "distilbert_qa_model.onnx"
+quantized_model_path = "distilbert_qa_model_int8.onnx"
 
 # Perform dynamic quantization to INT8
 quantize_dynamic(onnx_model_path, quantized_model_path, weight_type=QuantType.QInt8)
